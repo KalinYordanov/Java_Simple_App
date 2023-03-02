1b1606f7e9ae918cd2335e430a991237c56ddc86
KalinYordanov <kalinyordanov920815@gmail.com>
2023-03-02 19:13:52 +0200

    Create GameFrame.java

---
+++ PongGame/GameFrame.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+//window frame /
+
+public class GameFrame extends JFrame {
+
+    GamePanel panel;
+
+    GameFrame() {
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong Game");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
